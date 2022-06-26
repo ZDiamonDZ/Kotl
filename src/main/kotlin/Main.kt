20fd70ffd784084018d0e34d0220d135068d483d
Картушин Александр Николаевич <Kartushin_A@utkonos.ru>
2022-06-26 10:17:50 +0400

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args : Array<String>) {
+    val cl = Employee(fgs = "df", se = "df")
+    println(cl.se)
+}
+
+class Employee (var fgs : String) {
+    lateinit var se : String
+    constructor(se : String, fgs: String) : this(fgs) {
+    this.se = se
+    }
+
+}
